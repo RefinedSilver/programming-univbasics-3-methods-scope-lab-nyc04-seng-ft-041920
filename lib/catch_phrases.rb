3ce7b0fdd99f7c8daf4b0efbdd4d72c19b203f06
Ian McKenzie <imckenzie777@gmail.com>
2020-04-26 15:40:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,19 @@
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase "It's-a me, Mario!"
+  $status
+
   puts phrase
 end
-
-def toadstool
-  puts status
+def toadstool status
+  puts $status
 end
+def link
+  phrase = "it's dangerous to go alone! take this.".capitalize
+end
+def all_phrases
+  mario;
+  toadstool;
+  plink;
+end
+puts all_phrases
